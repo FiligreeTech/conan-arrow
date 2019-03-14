61fe1a494c2cfe5afb0ead207a92137b7f1b4816
Tom Walker <tom.walker@filigree.tech>
2019-03-14 12:01:16 +0000

    Windows - remove MT settings

--- build.py
+++ build.py
@@ -5,4 +5,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     #For c projects set pure_c to True to avoid building verisons for both libstdc++ and libc++
     builder.add_common_builds(pure_c=False)
+    builder.remove_build_if(lambda build: build.settings.get("compiler.runtime") == "MT")
+    builder.remove_build_if(lambda build: build.settings.get("compiler.runtime") == "MTd")
     builder.run()
